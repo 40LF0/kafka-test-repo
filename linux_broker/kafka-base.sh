d4027df3900842baa2f45cb37ef20830554033f5
40LF0 <wjdac66@gmail.com>
2023-12-29 23:44:47 +0900

    [feat] shell script for intial machine

--- linux_broker/kafka-base.sh
+++ linux_broker/kafka-base.sh
@@ -1,8 +1,8 @@
+#!/bin/sh
 wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add - 
 sudo add-apt-repository 'deb https://apt.corretto.aws stable main'
 sudo apt-get update; sudo apt-get install -y java-11-amazon-corretto-jdk
 wget https://archive.apache.org/dist/kafka/3.6.1/kafka_2.13-3.6.1.tgz
 tar xzf kafka_2.13-3.6.1.tgz
 rm kafka_2.13-3.6.1.tgz
-~/kafka_2.13-3.6.1/bin/zookeeper-server-start.sh ~/kafka_2.13-3.6.1/config/zookeeper.properties -daemon
-~/kafka_2.13-3.6.1/bin/kafka-server-start.sh ~/kafka_2.13-3.6.1/config/server.properties
+mv kafka_2.13-3.6.1 /root
---
+++ linux_broker/kafka-with-background-zookeeper.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add - 
+sudo add-apt-repository 'deb https://apt.corretto.aws stable main'
+sudo apt-get update; sudo apt-get install -y java-11-amazon-corretto-jdk
+wget https://archive.apache.org/dist/kafka/3.6.1/kafka_2.13-3.6.1.tgz
+tar xzf kafka_2.13-3.6.1.tgz
+rm kafka_2.13-3.6.1.tgz
+mv kafka_2.13-3.6.1 /root
+/root/kafka_2.13-3.6.1/bin/zookeeper-server-start.sh -daemon /root/kafka_2.13-3.6.1/config/zookeeper.properties
